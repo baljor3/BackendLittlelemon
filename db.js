f1676b83e0d5e9998a823629c4a174a78d7082c3
baljor3 <baljor20@gmail.com>
2024-01-07 21:40:35 -0800

    fix backend

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
-
-
 const pg = require('pg')
 
 const { Pool } = pg;
 const mysql = require("mysql")
+require('dotenv').config();
 
 
 const db = new Pool({
@@ -20,7 +19,14 @@ const db = mysql.createConnection({
   });
 */
   //connect
- 
+db.connect((err)=>{
+  if(err){
+  console.log(err)
+  throw err;
+}
+console.log("Connected to mysql");
+}
+)
 
   process.on('exit', () => {
     db.end();
